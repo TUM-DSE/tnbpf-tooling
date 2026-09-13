b50f86a79f38b5f92709de83fae64803721d1cbe
Deniz Mersinlioğlu <denizmersinlioglu2005@gmail.com>
2026-09-13 17:11:11 +0200

    Add pretty printing of byte sizes, credit StackOverflow properly

--- testing.py
+++ testing.py
@@ -3,6 +3,28 @@ from typing import Dict
 
 import parsing.parsing
 
+# https://stackoverflow.com/a/31631711
+def humanbytes(B):
+    """Return the given bytes as a human friendly KB, MB, GB, or TB string."""
+    B = float(B)
+    KB = float(1024)
+    MB = float(KB ** 2) # 1,048,576
+    GB = float(KB ** 3) # 1,073,741,824
+    TB = float(KB ** 4) # 1,099,511,627,776
+
+    if B < KB:
+        return '{0} B'.format(B)
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B / KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B / MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B / GB)
+    else:
+        return '{0:.2f} TB'.format(B / TB)
+
+
+
 def pretty_format(d, depth=0, startindent=True):
     if isinstance(d, dict):
         s = (" " * depth if startindent else "") + "{\n"
@@ -25,15 +47,16 @@ def pretty_print(p):
 def pretty_print_sizes(d: Dict[str, int]):
     program_size = float(d["program_size"])
     total_size = d["total_size"]
+    print(f"Total binary size: {humanbytes(total_size)}")
+    print(f"Program text section size: {humanbytes(program_size)} ({((program_size * 100.0) / total_size):.2f}% of binary)")
     print("Section\t\t\t\t\t\t\t\tSize in bytes\t\tSize wrt. code segment\t\tPercent of binary")
     metadata_footprint = 0
     for name, size in d.items():
-        if name == "total_size":
+        if not name.startswith("pcsection"):
             continue
-        if name.startswith("pcsection"):
-            metadata_footprint += size
-        print(f"{name:<32}\t{size}\t\t\t\t\t{((size * 100.0) / program_size):.2f}%\t\t\t\t\t\t{((size * 100.0) / total_size):.2f}%")
-    print(f"{"Total Metadata Footprint":<32}\t{metadata_footprint}\t\t\t\t\t{((metadata_footprint * 100.0) / program_size):.2f}%\t\t\t\t\t\t{((metadata_footprint * 100.0) / total_size):.2f}%")
+        metadata_footprint += size
+        print(f"{name:<32}\t{humanbytes(size)}\t\t\t\t{((size * 100.0) / program_size):.2f}%\t\t\t\t\t\t{((size * 100.0) / total_size):.2f}%")
+    print(f"{"Total Metadata Footprint":<32}\t{humanbytes(metadata_footprint)}\t\t\t\t{((metadata_footprint * 100.0) / program_size):.2f}%\t\t\t\t\t\t{((metadata_footprint * 100.0) / total_size):.2f}%")
 def gather(test: unittest.TestCase, name):
     symbounds, loopdb, functiondb, ivdb, sym_instr_db, sizes = parsing.parsing.get_and_parse(name)
     #test.assertIsNotNone(symbounds)
@@ -92,6 +115,16 @@ class EdgecaseUnsigned(unittest.TestCase):
         pretty_print(gather(self, self.src))  # add assertion here
 class SignedUnsigned(unittest.TestCase):
     src = "signed_unsigned.bpf.o"
+    def test_something(self):
+        print("[" + self.__class__.__name__ + "]")
+        pretty_print(gather(self, self.src))  # add assertion here
+class PointerLoop(unittest.TestCase):
+    src = "pointerloop.bpf.o"
+    def test_something(self):
+        print("[" + self.__class__.__name__ + "]")
+        pretty_print(gather(self, self.src))  # add assertion here
+class VariableStride(unittest.TestCase):
+    src = "variable_stride.bpf.o"
     def test_something(self):
         print("[" + self.__class__.__name__ + "]")
         pretty_print(gather(self, self.src))  # add assertion here
